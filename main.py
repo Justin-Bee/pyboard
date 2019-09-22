666c91e9dee865e61fdc08812ed5157e159b357c
Justin-Bee <jbee.appz@gmail.com>
2019-09-22 18:15:35 -0400

    switch fun

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pyb
 #import Pin so we can use it in the project
 from pyb import Pin
+from pyb import Switch
 
 #set p_out to Pin X1
 p_out = Pin('X1', Pin.OUT_PP)
@@ -8,6 +9,11 @@ p_out = Pin('X1', Pin.OUT_PP)
 #just a variable so the loop never stops
 x=0
 #while loop to continually blink the led in Pin X1
+
+sw = Switch()
+#when user switch is pressed the LED toggles on or off
+sw.callback(lambda: pyb.LED(3).toggle())
+
 while x==0:
      p_out.high()
      pyb.delay(500)
